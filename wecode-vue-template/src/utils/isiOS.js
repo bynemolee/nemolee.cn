8f770726fd402edaae09001ad10cc26584ffb660
nemo <small.p1102@gmail.com>
2023-11-08 13:49:32 +0800

    test

--- wecode-vue-template/src/utils/isiOS.js
+++
@@ -1,6 +0,0 @@
-/**
- * 是否是安卓
- */
-const isIOS = /iP(hone|od|ad)/.test(navigator.userAgent);
-
-export default isIOS;
--- wecode-vue-template/src/views/Desc/Subpage2.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <li>
-    基于 npm + webpack + react + redux + less + jsapi + weui（mit license）的快速开发本地化的框架。
-  </li> </template
->;
