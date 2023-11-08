e0ea6d7253b199b9f3e8c8ef026b096da4f45c26
nemo <small.p1102@gmail.com>
2023-11-08 13:54:14 +0800

    vue

---
+++ wecode-vue-template/src/views/VueTemplateIntro.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="weui-article">
+    <h1>Wecode Vue 模板说明</h1>
+    <h2>
+      参照
+      <router-link to="/desc" class="desc-link"> Wecode Reat模板 </router-link>
+      实现的第三方模板。
+    </h2>
+    <ul class="advantage">
+      <li>基于Vue-cli3.0进行制作。</li>
+      <li>遵循Wlk-cli集成规范。</li>
+      <li>允许开发者自由定制。</li>
+    </ul>
+  </div>
+</template>
+<style lang="less" scoped>
+.advantage {
+  list-style: circle;
+  padding-left: 20px;
+
+  li {
+    margin-bottom: 10px;
+  }
+}
+.desc-link {
+  color: #2b88e6;
+}
+</style>
